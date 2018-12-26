90c4a4a2e23105fa94bfe8b2b7d48b3f590b1d66
chandanasri232 <chandanasri232@gmail.com>
2018-12-26 15:47:10 +0530

    froms ction6 to github

--- models/store.py
+++ models/store.py
@@ -1,28 +1,28 @@
 from db import db
 
-class StoreModel(db.model):
+class StoreModel(db.Model):
+    __tablename__ = 'stores'
 
-	__tablename__ = 'stores'
-	
-	id = db.Column(db.Integer,primary_key=True)
-	name = db.Column(db.string(80))
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
 
-	items = db.relationship('ItemModel',lazy='dynamic')
+    items = db.relationship('ItemModel', lazy='dynamic')
 
-	def __init__(self,name):
-	self.name = name
-	
-	def json(self):
-		return {"name":self.name,"items":[item.json() for item in self.items.all()]}
-	
-	@classmethod
-	def find_by_naem(cls,name):
-		return cls.query.filter_by(name=name).first()
+    def __init__(self, name,id):
+        self.name = name
+        self.id = id
 
-	def save_to_db(self):
-		db.session.add(self)
-		db.session.commit()
-	
-	def delete_from_db(self):
-		db.session.delete(self)
-		db.session.commit()
+    def json(self):
+        return {'name': self.name, 'id' : self.id, 'items': [item.json() for item in self.items.all()]}
+
+    @classmethod
+    def find_by_name(cls, name):
+        return cls.query.filter_by(name=name).first()
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
--- resources/store.py
+++ resources/store.py
@@ -1,39 +1,38 @@
-from flask_restful import Resource,reqparse
-from models.item import ItemModel
+from flask_restful import Resource, reqparse
+from models.store import StoreModel
 
 class Store(Resource):
-	
-	def get(self,name):
-		
-		store = StoreModel.find_by_name(name)
-		if store:
-			return store.json()
-		
-		return {"message":"This store don't exist"},404
-	
-		
-	def post(self,name):
-			
-		if StoreModel.find_by_name(name):
-			return{"messgae":"An item '{}' alredy exists in the items list".format(name)}
-				
-		store= StoreModel(name)
-		try:
-			store.save_to_db()
-		except:
-			return{"message":"Error occurred while saving to db"},500
-		
-		return store.json(),201
+    parser = reqparse.RequestParser()
+    parser.add_argument('id',
+        type=int,
+        required=True,
+        help="Every store needs a store id.")
+    
+    def get(self, name):
+        store = StoreModel.find_by_name(name)
+        if store:
+            return store.json()
+        return {'message': 'Store not found'}, 404
 
-	
-	def delete(self,name):
-		store = StoreModel.find_by_name(name)		
-		if store: 
-			store.delete_from_db()
-			return{"message":"store deleted"}	
-		return{"message":"'{}' store is not present in the stores list".format(name)}
+    def post(self, name):
+        if StoreModel.find_by_name(name):
+            return {'message': "A store with name '{}' already exists.".format(name)}, 400
+        
+        data = Store.parser.parse_args() 
+        store = StoreModel(name, **data)
+        
+        store.save_to_db()
+       
+
+        return store.json(), 201
+
+    def delete(self, name):
+        store = StoreModel.find_by_name(name)
+        if store:
+            store.delete_from_db()
+
+        return {'message': 'Store deleted'}
 
 class StoreList(Resource):
-	def get(self):
-		return {'stores': [store.json() for store in StoreModel.query.all()]}
-		
+    def get(self):
+        return {'stores': [store.json() for store in StoreModel.query.all()]}
